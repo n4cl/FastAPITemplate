f7dc2fb98dd00a8e7b6106a0da0ed1b1d7a485da
n4cl <devn4cl@gmail.com>
2022-01-11 03:38:55 +0900

    Update: 入力パラメータを表示するように更新

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+
+@app.post("/")
+async def post(body=Body(...)):
+    print(body)
+    return  {"message": "Input data has been accepted"}
 
 
 @app.get("/hello")
