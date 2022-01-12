0db78b4241e30d9d5d0ada8185d779b6c43070be
n4cl <devn4cl@gmail.com>
2022-01-13 00:41:51 +0900

    Add: ログ出力を追加

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Body
+from logging_template.logging.logger import get_logger
 
 app = FastAPI()
+logger = get_logger(__name__)
 
 
 @app.post("/")
 async def post(body=Body(...)):
-    print(body)
+    logger.info(body)
     return  {"message": "Input data has been accepted"}
 
 
